f2b83e66435a8052487436da837f7d293656fd5d
sahil <sahilgulati49@gmail.com>
2019-03-07 19:21:32 +0530

    multi vendor, custom product type and more

--- direct-checkout.php
+++ direct-checkout.php
@@ -158,3 +158,249 @@ function sg_not_approved_privacy() {
         wc_add_notice( __( 'Please acknowledge the Privacy Policy' ), 'error' );
     }
 }
+
+// shipping
+//add_filter( 'woocommerce_package_rates', 'bbloomer_woocommerce_tiered_shipping', 999, 2 );
+//
+//function bbloomer_woocommerce_tiered_shipping( $rates, $package ) {
+//    $cart_weight = WC()->cart->cart_contents_weight;
+//    if ( isset( $rates['flat_rate:84'] ) ) {
+//        $rates['flat_rate:84']->cost = 5 * round ( $cart_weight );
+//    }
+//    return $rates;
+//}
+
+add_filter( 'woocommerce_package_rates', 'bbloomer_woocommerce_tiered_shipping', 9999, 2 );
+
+function bbloomer_woocommerce_tiered_shipping( $rates, $package ) {
+
+    if ( WC()->cart->cart_contents_weight < 1 ) {
+
+        if ( isset( $rates['flat_rate:5'] ) ) unset( $rates['flat_rate:6'], $rates['flat_rate:8'] );
+
+    } elseif ( WC()->cart->cart_contents_weight < 5 ) {
+
+        if ( isset( $rates['flat_rate:5'] ) ) unset( $rates['flat_rate:5'], $rates['flat_rate:8'] );
+
+    } else {
+
+        if ( isset( $rates['flat_rate:5'] ) ) unset( $rates['flat_rate:5'], $rates['flat_rate:6'] );
+
+    }
+
+    return $rates;
+
+}
+
+// Edit Single Product Page and shop page Add to Cart when product already in cart
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'bbloomer_custom_add_cart_button_single_product' );
+
+function bbloomer_custom_add_cart_button_single_product( $label ) {
+
+    foreach( WC()->cart->get_cart() as $cart_item_key => $values ) {
+        $product = $values['data'];
+        if( get_the_ID() == $product->get_id() ) {
+            $label = __('Already in Cart. Add again?', 'woocommerce');
+        }
+    }
+
+    return $label;
+
+}
+
+
+// Edit Loop Pages Add to Cart
+
+add_filter( 'woocommerce_product_add_to_cart_text', 'bbloomer_custom_add_cart_button_loop', 99, 2 );
+
+function bbloomer_custom_add_cart_button_loop( $label, $product ) {
+
+    if ( $product->get_type() == 'simple' && $product->is_purchasable() && $product->is_in_stock() ) {
+
+        foreach( WC()->cart->get_cart() as $cart_item_key => $values ) {
+            $_product = $values['data'];
+            if( get_the_ID() == $_product->get_id() ) {
+                $label = __('Already in Cart. Add again?', 'woocommerce');
+            }
+        }
+
+    }
+
+    return $label;
+
+}
+
+
+
+// multi vendor
+add_action( 'woocommerce_product_options_pricing', 'bbloomer_add_seller_to_products' );
+
+function bbloomer_add_seller_to_products() {
+
+    woocommerce_wp_text_input( array(
+            'id' => 'seller',
+            'class' => 'short',
+            'label' => __( 'Seller', 'woocommerce' )
+        )
+    );
+
+}
+
+add_action( 'save_post', 'bbloomer_save_seller' );
+
+function bbloomer_save_seller( $product_id ) {
+
+    if ( isset( $_POST['seller'] ) ) {
+        update_post_meta( $product_id, 'seller', $_POST['seller'] );
+    }
+
+}
+
+add_action( 'woocommerce_single_product_summary', 'bbloomer_display_seller', 25 );
+
+function bbloomer_display_seller() {
+
+    global $product;
+
+    if ( $seller = get_post_meta( $product->get_id(), 'seller', true ) ) {
+        echo '
+<div class="woocommerce_seller"><b>';
+        _e( 'Seller: ', 'woocommerce' );
+        echo '<span>' . $seller . '</span>';
+        echo '</b></div>
+ 
+';
+    }
+
+}
+
+
+//// #1 Add New Product Type to Select Dropdown
+//
+//add_filter( 'product_type_selector', 'bbloomer_add_custom_product_type' );
+//
+//function bbloomer_add_custom_product_type( $types ){
+//    $types[ 'custom' ] = 'Custom product';
+//    return $types;
+//}
+//
+//// --------------------------
+//// #2 Add New Product Type Class
+//
+//add_action( 'init', 'bbloomer_create_custom_product_type' );
+//
+//function bbloomer_create_custom_product_type(){
+//    class WC_Product_Custom extends WC_Product {
+//        public function get_type() {
+//            return 'custom';
+//        }
+//    }
+//}
+//
+//// --------------------------
+//// #3 Load New Product Type Class
+//
+//add_filter( 'woocommerce_product_class', 'bbloomer_woocommerce_product_class', 10, 2 );
+//
+//function bbloomer_woocommerce_product_class( $classname, $product_type ) {
+//    if ( $product_type == 'custom' ) {
+//        $classname = 'WC_Product_Custom';
+//    }
+//    return $classname;
+//}
+
+// custom product type in woocommerce
+add_action( 'init', 'register_demo_product_type' );
+
+function register_demo_product_type() {
+
+    class WC_Product_Demo extends WC_Product {
+
+        public function __construct( $product ) {
+            $this->product_type = 'demo';
+            parent::__construct( $product );
+        }
+    }
+}
+
+
+add_filter( 'product_type_selector', 'add_demo_product_type' );
+
+function add_demo_product_type( $types ){
+    $types[ 'demo' ] = __( 'Demo product', 'dm_product' );
+
+    return $types;
+}
+
+add_action( 'woocommerce_product_data_panels', 'demo_product_tab_product_tab_content' );
+
+add_filter( 'woocommerce_product_data_tabs', 'demo_product_tab' );
+
+function demo_product_tab( $tabs) {
+
+    $tabs['demo'] = array(
+        'label'    => __( 'Demo Product', 'dm_product' ),
+        'target' => 'demo_product_options',
+        'class'  => 'show_if_demo_product',
+    );
+    return $tabs;
+}
+
+function demo_product_tab_product_tab_content()
+{
+
+    ?>
+    <div id='demo_product_options' class='panel woocommerce_options_panel'><?php
+    ?>
+    <div class='options_group'><?php
+
+    woocommerce_wp_text_input(
+        array(
+            'id' => 'demo_product_info',
+            'label' => __('Demo Product Spec', 'dm_product'),
+            'placeholder' => '',
+            'desc_tip' => 'true',
+            'description' => __('Enter Demo product Info.', 'dm_product'),
+            'type' => 'text'
+        )
+    );
+        woocommerce_wp_text_input(
+            array(
+                'id' => 'demo_product_info2',
+                'label' => __('Demo Product Spec1', 'dm_product1'),
+                'placeholder' => '',
+                'desc_tip' => 'true',
+                'description' => __('Enter Demo product Info.', 'dm_product1'),
+                'type' => 'text'
+            )
+        );
+    ?></div>
+    </div><?php
+}
+
+
+add_action( 'woocommerce_process_product_meta', 'save_demo_product_settings' );
+
+function save_demo_product_settings( $post_id ){
+
+    $demo_product_info = $_POST['demo_product_info'];
+
+    if( !empty( $demo_product_info ) ) {
+
+        update_post_meta( $post_id, 'demo_product_info', esc_attr( $demo_product_info ) );
+    }
+}
+
+add_action( 'woocommerce_single_product_summary', 'demo_product_front' );
+
+function demo_product_front () {
+    global $product;
+
+    if ( 'demo' == $product->get_type() ) {
+        echo( get_post_meta( $product->get_id(), 'demo_product_info' )[0] );
+
+    }
+}
+
+
